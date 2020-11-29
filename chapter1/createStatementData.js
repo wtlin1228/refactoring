8db5868377e0f4540837c27a567f930a65c72a66
wtlin <wtlin1228@gmail.com>
2020-11-29 11:16:59 +0800

    move amount and volumeCredits into class

--- chapter1/createStatementData.js
+++ chapter1/createStatementData.js
@@ -22,57 +22,22 @@ function totalVolumeCredits(data) {
 }
 
 function enrichPerformance(aPerformance, plays) {
-  const calculator = new PerformanceCalculator(aPerformance);
+  const calculator = new PerformanceCalculator(
+    aPerformance,
+    playFor(aPerformance, plays)
+  );
 
   const result = Object.assign({}, aPerformance);
 
   result.play = playFor(result, plays);
-  result.amount = amountFor(result);
-  result.volumeCredits = volumeCreditsFor(result);
+  result.amount = calculator.amount;
+  result.volumeCredits = calculator.volumeCredits;
 
   return result;
 }
 
 function playFor(aPerformance, plays) {
   return plays[aPerformance.playID];
-}
-
-function amountFor(aPerformance) {
-  let result = 0;
-
-  switch (aPerformance.play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-
-    default:
-      throw new Error(`unknown type: ${aPerformance.play.type}`);
-  }
-
-  return result;
-}
-
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-
-  result += Math.max(aPerformance.audience - 30, 0);
-
-  if ("comedy" === aPerformance.play.type) {
-    result += Math.floor(aPerformance.audience / 5);
-  }
-
-  return result;
 }
 
 module.exports = { createStatementData };
