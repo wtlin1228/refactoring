365072335375b52620a431872eec9d76bc670db8
wtlin <wtlin1228@gmail.com>
2020-11-29 08:59:37 +0800

    separate data collection and render logic

--- chapter1/statement.js
+++ chapter1/statement.js
@@ -1,86 +1,30 @@
-function statement(invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
+const { createStatementData } = require("./createStatementData");
 
-  for (let perf of invoice.performances) {
+function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
+}
+
+function renderPlainText(data) {
+  let result = `Statement for ${data.customer}\n`;
+
+  for (let perf of data.performances) {
     // 印出這份訂單
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
+    result += `${perf.play.name}: ${usd(perf.amount)} (${
       perf.audience
     }) seats\n`;
   }
 
-  result += `Amount owed is ${usd(totalAmount(invoice))}\n`;
-  result += `You earned ${totalVolumeCredits(invoice)} credits\n`;
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
+}
 
-  function amountFor(aPerformance) {
-    let result = 0;
-
-    switch (playFor(aPerformance).type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-
-      default:
-        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }
-
-    return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-
-    result += Math.max(aPerformance.audience - 30, 0);
-
-    if ("comedy" === playFor(aPerformance).type) {
-      result += Math.floor(aPerformance.audience / 5);
-    }
-
-    return result;
-  }
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
-
-  function totalVolumeCredits(invoice) {
-    let result = 0;
-
-    for (let perf of invoice.performances) {
-      result += volumeCreditsFor(perf);
-    }
-
-    return result;
-  }
-
-  function totalAmount(invoice) {
-    let result = 0;
-
-    for (let perf of invoice.performances) {
-      result += amountFor(perf);
-    }
-
-    return result;
-  }
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
 
 module.exports = {
